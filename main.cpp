15651936d755a2cda6d6eea58ee9fa690c927561
Tanishq Gadkari <gadkaritanishq@gmail.com>
2020-03-06 23:28:36 -0800

    second commit

--- main.cpp
+++ main.cpp
@@ -1,88 +1,54 @@
-#include <iostream>
-#include <string>
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-void print(ListNode* &head, int counter)
-{
-    ListNode* temp = head;
-    for(int i = 0; i < counter; ++i)
-    {
-        std::cout << temp->val << " ";
-        temp = temp->next;
-    }
-   
-    std::cout << std::endl;
-}
-
-void insertBeginning(ListNode* &head, int val, int &counter)
-{
-    ListNode* temp = head;
-    ListNode* insert = new ListNode(val);
-    if(head == nullptr)
-    {
-        head = insert;
-        ++counter; 
-        return;
-    }
-    //std::cout << "head is not empty";
-    head = insert;
-    head->next = temp;
-    ++counter;
-    
-}
-
-ListNode* addTwoNumbers(ListNode*, ListNode*);
-
-int main()
-{
-    ListNode* head1;
-    ListNode* head2;
-    ListNode* sol;
-    int count3 = 0;
-    int count1 = 0; int count2 = 0;
-    insertBeginning(head1, 4, count1);
-    insertBeginning(head1, 4, count1);
-    insertBeginning(head1, 2, count1);
-
-    insertBeginning(head2, 6, count2);
-    insertBeginning(head2, 6, count2);
-    insertBeginning(head2, 5, count2);
-    print(head1, count1);
-    print(head2, count2);
-    sol = addTwoNumbers(head1, head2);
-    print(sol, 3);
-    return 0;
-}
-
-ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
-{
-    ListNode* solution;
-    int sum = 0;
-    int reminder = 0;
-    int increment = 0;
-    int count;
-    while(l1 != nullptr && l2 != nullptr)
-    {
-        sum = l1->val + l2->val;
-        reminder = sum % 10;
-        if(reminder != 0)
-        {
-            insertBeginning(solution, reminder + increment, count);
-            if(increment != 1)
-                ++increment;
-        }
-        else
-        {
-            insertBeginning(solution, sum + increment, count);
-                increment = 0;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = l1;
+    while(l1 != nullptr && l2 != nullptr){
+        l1->val += l2->val;
+        if(l1->val >= 10){
+            l1->val = l1->val % 10;
+            if(l1->next != nullptr)
+                l1->next->val++;
+            else
+                l1->next = new ListNode(1);
         }
         l1 = l1->next;
         l2 = l2->next;
     }
-    return solution;
-}
+    ListNode* temp = l1;
+    while(temp != nullptr)
+    {
+        if(temp->val >= 10){
+            temp->val = temp->val % 10;
+            if(temp->next != nullptr)
+                temp->next->val++;
+            else
+                temp->next = new ListNode(1);
+        }
+        temp = temp->next;
+    }
+    
+    
+    temp = head;
+    while(temp->next != nullptr)
+        temp = temp->next;
+
+    while(l2 != nullptr){
+        l1 = new ListNode(l2->val);
+        temp->next = l1;
+        temp = temp->next;
+        l1 = l1->next;
+        l2 = l2->next;
+    }
+    return head;
+
+    }
+    
+};
